1796a803da201d0d78cf65bc0e652f14f47c3abd
deadpool1994 <mhtrami@gmail.com>
2016-11-25 13:36:24 +0530

    Harder, Better, Stronger Faster

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,4 +11,26 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', [function() {
 
+  $(document).ready(function(){
+    console.log('hello');
+    $(".playvideo").click(function(){
+      console.log('document');
+      $(".bound2")[0].src += "&autoplay=1";
+      $(".bound3")[0].src += "&autoplay=1";
+      $(this).unbind("click");//or some other way to make sure that this only happens once
+     });
+    var refreshId = setTimeout(function(){
+    // if(!$('.ytp-large-play-button.ytp-button')){
+    //   console.log('hmmmk');
+    //   $('.ytp-large-play-button.ytp-button').style.display = "none";
+    //   clearInterval(refreshId);
+    // }else{
+    //   console.log('kmmmh');
+    // }
+    var x =document.body.childNodes;
+    children = document.querySelectorAll('.ytp-thumbnail-overlay-image');
+    children[0].style.zIndex = "1";
+}, 5000);
+
+  });
 }]);
